d41af0d81e47550a4a8201cb4c63d6bf202777a2
PixelsCommander <denis.radin@gmail.com>
2017-10-21 10:16:57 +0200

    Step 2 - Drawing a sector

--- fragment-shader.glsl
+++ fragment-shader.glsl
@@ -16,8 +16,34 @@ float circle(vec2 coord, vec2 center, float radius) {
   return step(distanceToCenter, radius);
 }
 
+bool isAngleBetween(float target, float angle1, float angle2) {
+  float startAngle = min(angle1, angle2);
+  float endAngle = max(angle1, angle2);
+
+  if (endAngle - startAngle < 0.1) {
+    return false;
+  }
+
+  target = mod((360. + (mod(target, 360.))), 360.);
+  startAngle = mod((3600000. + startAngle), 360.);
+  endAngle = mod((3600000. + endAngle), 360.);
+
+  if (startAngle < endAngle) return startAngle <= target && target <= endAngle;
+  return startAngle <= target || target <= endAngle;
+}
+
+float sector(vec2 coord, vec2 center, float startAngle, float endAngle) {
+  vec2 uvToCenter = coord - center;
+  float angle = degrees(atan(uvToCenter.y, uvToCenter.x));
+  if (isAngleBetween(angle, startAngle, endAngle)) {
+    return 1.0;
+  } else {
+    return 0.;
+  }
+}
+
 void main() {
   vec2 coord = vec2(gl_FragCoord);
-  float isFilled = circle(coord, center, radius);
+  float isFilled = circle(coord, center, radius) * sector(coord, center, 0., 75.);
   gl_FragColor = vec4(1. - isFilled);
 }
